fd45f636cb5db5b2fe3db6301b5cb4b583b36e6a
yuwankang <kangyuwan@naver.com>
2020-12-05 21:12:17 +0900

    버튼 만들기 내리기 리셋 추가
    methods: 를 사용하여 버튼에 데이터값이 들어갔다.
    v-onclick을 @click으로 대체 가능하다.

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,6 +2,8 @@
   <div>
     <p>카운터! {{ cnt }}</p>
     <button @click="up">올리기</button>
+    <button @click="down">내리기</button>
+    <button @click="reset">리셋</button>
   </div>
 </template>
 
@@ -16,6 +18,12 @@ export default {
     up() {
       this.cnt++
     },
+    down(){
+      this.cnt--
+    },
+    reset(){
+      this.cnt=100
+    }
   }
 }
 </script>
