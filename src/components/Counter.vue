d84706f6652dd52db47d4d646560b1b3faae9181
mihye <mihye0924@naver.com>
2020-12-05 20:48:45 +0900

    cnt down, cnt reset add
    20201205 커밋

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,21 +1,30 @@
 <template>
   <div>
     <p>카운터! {{ cnt }}</p>
-    <button @click="up">올리기</button>
-  </div>
+    <button @click="up">올리기</button>  
+    <button @click="down">내리기</button>  
+    <button @click="reset">초기화</button> 
+  </div> 
+ 
 </template>
-
 <script>
 export default {
-  data() {
-    return {
-      cnt: 100
-    }
-  },
-  methods: {
-    up() {
-      this.cnt++
+    data() {
+      return {
+        cnt: 100
+      }
     },
+   
+    methods: {
+      up() {
+        this.cnt++ 
+    },
+      down(){
+      this.cnt--
+    },  
+     reset(){ 
+     this.cnt=100
+    }, 
   }
 }
-</script>
+</script> 
